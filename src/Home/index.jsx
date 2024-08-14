c558fdc832ac98006c3fd7124a72099d1ff2c41e
VictorHugoGomesCassunde <victorhugocassunde10@gmail.com>
2024-08-14 16:06:22 -0400

    14/08
    terminar

---
+++ src/Home/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+import { Carousel } from 
+
+export default function Home(){
+    return(
+        <>
+        <Header/>
+        <p>Minha Home</p>
+
+        </>
+    )
+}
